77b6cca53dc5c288e941cb7ad364a6f5c4ca5b86
Nguyen Thanh Tuan <ngthtuan333@gmail.com>
2023-10-01 01:22:57 +0700

    Done

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import FirstScreen from "./UI/FirstScreen";
-import Ui1a from "./UI/1_a";
-import Ui1b from "./UI/1_b";
-import Ui1c from "./UI/1_c";
-import Ui1d from "./UI/1_d";
-import Ui1e from "./UI/1_e";
-import Ui2a from "./UI/2_a";
-import XMEye from "./UI/XMEye"
+import FirstScreen from './UI/FirstScreen';
+import Ui1a from './UI/1_a';
+import Ui1b from './UI/1_b';
+import Ui1c from './UI/1_c';
+import Ui1d from './UI/1_d';
+import Ui1e from './UI/1_e';
+import Ui2a from './UI/2_a';
+import XMEye from './UI/XMEye';
 
 export default function App() {
-  return (
-    // <FirstScreen />
-    // <Ui1a/>
-    // <Ui1b/>
-    // <Ui1c/>
-    // <Ui1d/>
-    // <Ui1e/>
-    // <Ui2a/>
-    <XMEye/>
-  );
+    return (
+        // <FirstScreen />
+        // <Ui1a/>
+        // <Ui1b/>
+        // <Ui1c/>
+        // <Ui1d/>
+        // <Ui1e/>
+        // <Ui2a/>
+        <XMEye />
+    );
 }
-
